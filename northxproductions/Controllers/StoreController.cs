cf194792e244c856243f9dca0ab1b8a5a88a1cc4
Lucas Denk <denk.luca@uwlax.edu>
2017-10-12 15:08:47 -0500

    fuck you paul

---
+++ northxproductions/Controllers/StoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace northxproductions.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult StoreIndex()
+        {
+            return View();
+        }
+    }
+}
